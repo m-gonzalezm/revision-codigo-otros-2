ca7bb7043fb5e817f64e2e1332ff0a40cc7f013a
Miguel <Miguel_a.GM@hotmail.com>
2024-04-07 12:07:22 -0600

    fix: correciones y reestructuracion del codigo

--- formulario.js
+++ formulario.js
@@ -1,97 +1,71 @@
-var formulario = document.querySelector("#form")
+const formulario = document.querySelector(".formulario"); // Se cambió el selector y el nombre de la clase a la que hace referencia
 
-formulario.onsubmit = function(e) {
+formulario.onsubmit = function(event) { // Se renombró el parámetro para mejorar la legibilidad, es cuestión de gustos, nombrar e a un evento es aceptado
 
-  e.prevent();
+  event.preventDefault(); // Se corrigió el nombre del método preventDefault
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  const inputNombre = this.elements[0];
+  const inputEdad = this.elements[1];
+  const selector = this.elements[2];
+  /* Se renombraron n, e y na para hacerlas más descriptivas y mejorar su legibilidad. Se cambió el elemento de referencia por un this para mejorar la estructura de la función */
 
-  var nombre = n.value
-  var edad = e.value
+  let nombre = inputNombre.value;
+  let edad = inputEdad.value;
+  let nacionalidad = selector.options[selector.selectedIndex].textContent; // Se evitó la declaración del índice y se cambió value por textContent para su posterior uso
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
+  /* Se removieron los console.log por desuso */
 
-  if (nombre.length === 0) {
-    n.classList.add("error")
-  }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
-  }
+  inputNombre.classList.toggle("error", !nombre);
+  inputEdad.classList.toggle("error", edad < 18 || edad > 120);
+  /* Se refactorizaron las condicionales, haciendo uso del método toggle para incluir la remoción de la clase error */
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
+  if (nombre && edad >= 18 && edad <= 120) // Se removieron los paréntesis al existir únicamente el operador &&, se modificó el primer operando para reducir el código y se evitaron las llaves al ser una única instrucción
+    agregarInvitado(nombre, edad, nacionalidad);
+
 }
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
+// Se eliminó la sección del código que creaba un botón
 
 function agregarInvitado(nombre, edad, nacionalidad) {
 
-  if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
-  }
-  else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
-  }
-  else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
-  }
-  else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
+  /* Se removieron las condicionales porque nacionalidad ya llega con el texto que se va a renderizar */
+
+  const lista = document.getElementById("lista-de-invitados");
+
+  const elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista"); //Se corrigió el nombre del método add
+  lista.appendChild(elementoLista);
+  
+  function crearElemento(descripcion, valor) {
+    const spanNombre = document.createElement("span");
+    const inputNombre = document.createElement("input"); // Si no se está ocupando el input para posibles correcciones o modificaciones, se debe cambiar porque es inconsistente y puede causar problemas
+    spanNombre.textContent = descripcion + ": ";
+    inputNombre.value = valor;
+    elementoLista.appendChild(spanNombre);
+    elementoLista.appendChild(inputNombre);
+    elementoLista.appendChild(document.createElement("br")); // Se evitó la declaración del espacio porque no se manipula
   }
 
-var lista = document.getElementById("lista-de-invitados")
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
 
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
+  const botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
+  /* Se removió el id porque un botón se crea por cada invitado */
+  elementoLista.appendChild(document.createElement("br")); // Se evitó la declaración del espacio porque no se manipula
+  elementoLista.appendChild(botonBorrar);
+  
+  botonBorrar.onclick = function() {
+    this.parentNode.remove(); // Se cambió el elemento de referencia por un this para mejorar la estructura de la función
+  }
+  
+  formulario.reset(); // Se implementó el método reset al formulario para limpiar los valores una vez hecho submit
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
 }
 
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
-
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
-elementoLista.appendChild(botonBorrar);
-
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
-  }
-}
+/* 
+*   Se modificó la declaración a todas las variables por uno más acorde a su uso y alcance
+*   Se agregó ; al final de cada sentencia, por ser buena práctica y para ser consistentes en el código
+*   Se recomienda el uso de margin, padding para espaciados, en lugar de br
+*/
